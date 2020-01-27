a24d2d0233f2cccb3b82d02db5f4429f8d41e37b
Marco Paulo Ollivier <marcopollivier@gmail.com>
2020-01-27 19:34:47 -0300

    update personal data

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,25 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Build the project.
 hugo -t hugo-coder
 
+# Go To Public folder
 cd public
-
+# Add changes to git.
 git add .
-git commit -m "Rebuilding site `date`"
+
+# Commit changes.
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
 git push origin master
 
+# Come Back up to the Project Root
 cd ..
 
 git add .
